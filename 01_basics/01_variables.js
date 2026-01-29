cfdd42f060b1d4c28e1cafab7d20a471efd3aee5
prince raghav <raghavprince321@gmail.com>
2026-01-29 16:06:26 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144543
+let accountEmail = "prince@gmail.com"
+var accountPassword = "12345"
+// accountId = 2
+accountcity = "meerut"
+accountEmail = "princeraghav@gmail.com"
+accountPassword = "212121"
+accountCity = "hapur"
+let accountState ;
+
+
+console.log(accountId);
+console.table([accountId, accountcity , accountEmail , accountPassword, accountCity , accountState ])
+
+
+
