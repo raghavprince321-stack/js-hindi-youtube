b9a387d3933a9497bf57a9af952ae1773f7d2a03
prince raghav <raghavprince321@gmail.com>
2026-02-06 07:56:03 +0000

    date and time in javascript

---
+++ 01_basics/10_dateandtime.js
@@ -0,0 +1,44 @@
+// Dates
+/*
+let myDate = new Date()
+console.log(myDate); // 2026-02-06T07:15:50.1592
+console.log(myDate.toString()); // Fri Feb 06 2026 07:15:50 GMT+0000 (Coordinated
+console.log(myDate.toDateString()); //Fri Feb 06 2026
+console.log(myDate.toLocaleDateString()); // 2/6/2026
+console.log(typeof myDate); // object
+
+let myCreateDate = new Date(2023,0,23)
+console.log(myCreateDate.toDateString()); // Mon Jan 23 2023
+
+
+let myCreateDate = new Date("2026-02-06")
+//console.log(myCreateDate.toLocaleString()); // 1/6/2026, 12:00:00 AM
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp);
+//console.log(myCreateDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+
+console.log(newDate);
+console.log(newDate.getDate()); // 6
+console.log(newDate.getDay()); // 5
+console.log(newDate.getMonth() + 1); //2
+
+
+newDate.toLocaleString('default', {
+     weekday: "long",
+    }); 
+
+
+*/ 
+
+
+
+
+
+
+
+
