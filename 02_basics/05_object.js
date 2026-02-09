f20c6cdfea8a14ef97b0bda80a8dc744bb292ee4
prince raghav <raghavprince321@gmail.com>
2026-02-09 15:54:44 +0000

    json talks

---
+++ 02_basics/05_object.js
@@ -0,0 +1,42 @@
+// object de-structured and JSON API intro
+
+const course = {
+    coursename: "javascript",
+    price: 999,
+    courseinstructor: "hitesh"
+}
+
+//course.coursename 
+
+//const{courseinstructor} = course
+//console.log(courseinstructor); // hitesh
+
+//const{courseinstructor : instructor} = course
+//console.log(instructor); // hitesh
+/*
+const navbar = ({company}) => {} 
+
+navbar(company = "prince")
+console.log(company);
+*/
+
+//{
+//    "name": "prince",
+//    "coursename": "javascript",
+//    "price": "free"
+//}
+
+//json 
+[
+    {},
+    {},
+    {}
+]
+
+
+
+
+
+
+
+
